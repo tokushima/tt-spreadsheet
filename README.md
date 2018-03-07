d6ad23fec1534420ddd3770a55e93f3e58b093f8
tokushima <tokushima@192.168.103.193>
2018-03-07 12:08:51 +0900

    push

--- README.md
+++ README.md
@@ -1,1 +1,36 @@
 # tt-spreadsheet
+
+
+```
+
+$obj = new \tt\Spreadsheet();
+
+$obj->set_sheet_title('最初');
+$obj->set_cell_width(1,100);
+$obj->set_cell(1,1,'コード','FFCC33');
+$obj->set_cell(2,1,'日付','FFCC33');
+
+for($i=2;$i<1000;$i++){
+	$obj->set_cell(1,$i,'012345678901234567890'.sprintf('%04d',$i));
+	$obj->set_cell(2,$i,date('Y/m/d',time() + ($i * 86400)));
+}
+
+
+$obj->add_sheet('次');
+$obj->set_cell_width(1,100);
+$obj->set_cell(1,1,'コード','CCFF33');
+$obj->set_cell(2,1,'日付','CCFF33');
+
+for($i=2;$i<100;$i++){
+	$obj->set_cell(1,$i,'ABC'.sprintf('%04d',$i));
+	$obj->set_cell(2,$i,date('Y/m/d',time() + ($i * 86400)));
+}
+$obj->write('AAAAA.xlsx');
+
+
+
+$obj = new \tt\Spreadsheet('AAAAA.xlsx');
+$obj->active_sheet('次');
+var_dump($obj->get_cell(1,10));
+
+```
---
+++ lib/tt/Spreadsheet.php
@@ -0,0 +1,126 @@
+<?php
+namespace tt;
+/**
+ * 
+ * @composer phpoffice/phpspreadsheet 1.2.0
+ * @author tokushima
+ *
+ */
+class Spreadsheet{
+	private $spshee;
+	private $active_sheet;
+	
+	public function __construct($filename=null){
+		if(ini_get('date.timezone') == ''){
+			date_default_timezone_set('Asia/Tokyo');
+		}
+		if(!empty($filename)){
+			$reader = new \PhpOffice\PhpSpreadsheet\Reader\Xlsx();
+			$this->spshee = $reader->load($filename);
+		}else{
+			$this->spshee = new \PhpOffice\PhpSpreadsheet\Spreadsheet();
+		}
+		$this->active_sheet = $this->spshee->setActiveSheetIndex(0);
+	}
+	
+	/**
+	 * シートに名前をつける
+	 * @param string $title
+	 */
+	public function set_sheet_title($title){
+		$this->active_sheet->setTitle($title);
+		return $this;
+	}
+	
+	/**
+	 * シートを追加する
+	 * @param string $title
+	 */
+	public function add_sheet($title=null){
+		$this->active_sheet = $this->spshee->createSheet();
+		
+		if(!empty($title)){
+			$this->set_sheet_title($title);
+		}
+		return $this;
+	}
+	
+	/**
+	 * シートを選択する
+	 * @param number $index
+	 */
+	public function active_sheet($index=0){
+		if(is_int($index)){
+			$this->active_sheet = $this->spshee->setActiveSheetIndex($index);
+		}else{
+			$this->active_sheet = $this->spshee->setActiveSheetIndexByName($index);
+		}
+		return $this;
+	}
+	
+	/**
+	 * セルの幅を指定する
+	 * @param integer $column
+	 * @param integer $size
+	 */
+	public function set_cell_width($column,$size){
+		$this->active_sheet->getColumnDimensionByColumn($column)->setWidth($size);
+		return $this;
+	}
+	
+	/**
+	 * セルに値をセットする
+	 * @param integer $column
+	 * @param integer $row
+	 * @param mixed $value
+	 * @param string $bgrgb
+	 * @param number $width
+	 */
+	public function set_cell($column,$row,$value,$bgrgb=null,$width=0){
+		$this->active_sheet->setCellValueByColumnAndRow($column,$row,$value,\PhpOffice\PhpSpreadsheet\Cell\DataType::TYPE_STRING);
+
+		if(!empty($bgrgb)){
+			$this->active_sheet->getStyleByColumnAndRow($column,$row)->getFill()->setFillType(\PhpOffice\PhpSpreadsheet\Style\Fill::FILL_SOLID);
+			$this->active_sheet->getStyleByColumnAndRow($column,$row)->getFill()->getStartColor()->setRGB($bgrgb);
+		}
+		return $this;
+	}
+	
+	/**
+	 * セルの値を取得する
+	 * @param integer $column
+	 * @param integer $row
+	 * @return mixed
+	 */
+	public function get_cell($column,$row){
+		$cell = $this->active_sheet->getCellByColumnAndRow($column,$row);
+		return $cell->getValue();
+	}
+	
+	/**
+	 * attachmentとして出力する
+	 * @param string $filename
+	 */
+	public function output($filename=null){
+		if(empty($filename)){
+			$filename = date('YmdHis').'.xlsx';
+		}
+		header('Content-Type: application/octet-stream');
+		header('Content-Disposition: attachment;filename="'.$filename.'"');
+
+		$writer = new \PhpOffice\PhpSpreadsheet\Writer\Xlsx($this->spshee);
+		$writer->save('php://output');
+	}
+	
+	/**
+	 * xlsとしてファイルに書き出す
+	 * @param string $filename
+	 */
+	public function write($filename=null){
+		if(empty($filename)){
+			$filename = date('YmdHis').'.xlsx';
+		}
+		$writer = new \PhpOffice\PhpSpreadsheet\Writer\Xlsx($this->spshee);
+		$writer->save($filename);
+	}
+}
